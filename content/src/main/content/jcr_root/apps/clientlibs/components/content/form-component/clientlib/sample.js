52b7901f8692e92cedf361ea7a301a57a187dc61
Shashi Bhushan <sbhushan1@outlook.com>
2017-02-09 16:52:11 +0530

    CSRF - Stash Commit

---
+++ content/src/main/content/jcr_root/apps/clientlibs/components/content/form-component/clientlib/sample.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Shashi Bhushan on 1/2/2017.
+ */
+
+"use strict";
+
+;(function($){
+//    $(document).ready(function() {
+//        $('#submit-button').click(function(event){
+//            // Stop the form from submitting
+//            event.preventDefault();
+//
+//            $.ajax({
+//                url : '/bin/postPath.sample',
+//                method : 'post',
+//                success : function(data) {
+//                    console.log(data);
+//                    $('#prompt').hide();
+//                    $('#result').html(data);
+//                },
+//                error : function(cause) {
+//                    console.log(cause);
+//                    $('#prompt').hide();
+//                    $('#result').html(data);
+//                }
+//            });
+//        });
+//    });
+})(jQuery);
